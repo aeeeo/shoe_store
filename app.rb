82c6ee919a386482b8185ca28e3d3bc04430b79b
Andrew Peterson <Home@Andrew-Peterson.local>
2018-03-02 09:29:04 -0800

    add app.rb routing

--- app.rb
+++ app.rb
@@ -16,3 +16,59 @@ end
 post('/') do
   erb:index
 end
+
+get('/stores') do
+  erb:stores
+end
+
+post('/stores') do
+  erb:stores
+end
+
+get('stores/:id') do
+  erb:store_view
+end
+
+get('stores/:id/edit') do
+  erb:store_edit
+end
+
+patch('/stores/:id/edit') do
+  erb:store_edit
+end
+
+post('/stores/:id/add_brand') do
+  erb:store_edit
+end
+
+get('/brands') do
+  erb:brands
+end
+
+get('/brands/:id/stores') do
+  erb:brand
+end
+
+get('/shoes/:id/stores') do
+  erb:shoe
+end
+
+get('/shoes') do
+  erb:shoes
+end
+
+post('/stores/:id/add_brand') do
+  erb:store_edit
+end
+
+delete('/stores/:id/delete') do
+  erb:index
+end
+
+delete('/stores/:id/delete_brand/:brand_id') do
+  erb:store_edit
+end
+
+delete('/stores/:id/delete_shoes/:shoe_id') do
+  erb:store_edit
+end
--- lib/Shoe.rb
+++ lib/Shoe.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 class Shoe < ActiveRecord::Base
-  has_and_belongs_to_many :brands
   has_and_belongs_to_many :stores
+  belongs_to :brand
 end
