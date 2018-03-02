784a3dbd03ab9787c95d3b47f8722dde8963919e
Andrew Peterson <Home@Andrew-Peterson.local>
2018-03-02 08:41:27 -0800

    set up tables in database

---
+++ db/migrate/20180302163142_make_tables.rb
@@ -0,0 +1,26 @@
+class MakeTables < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:brands) do |t|
+      t.column(:name, :string)
+      t.timestamps
+    end
+    create_table(:shoes) do |t|
+      t.column(:name, :string)
+      t.timestamps
+    end
+    create_table(:stores) do |t|
+      t.column(:name, :string)
+      t.timestamps
+    end
+    create_table(:brands_shoes) do |t|
+      t.column(:brand_id, :integer)
+      t.column(:shoe_id, :integer)
+      t.timestamps
+    end
+    create_table(:brands_stores) do |t|
+      t.column(:brand_id, :integer)
+      t.column(:shoe_id, :integer)
+      t.timestamps
+    end
+  end
+end
